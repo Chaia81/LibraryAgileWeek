544e7ce499857ca8ed4ba7c9ad6f2b6bd6afe8c1
jacob <jacobbayp@gmail.com>
2020-10-09 10:46:42 +0200

    Merge branch 'Ida' of https://github.com/Chaia81/LibraryAgileWeek into jacob
     Conflicts:
    	src/main/java/dk/kea/dat19c/Library/java/models/boegerDTO.java

--- src/main/java/dk/kea/dat19c/Library/java/controllers/boegerController.java
+++ src/main/java/dk/kea/dat19c/Library/java/controllers/boegerController.java
@@ -1,15 +1,28 @@
 package dk.kea.dat19c.Library.java.controllers;
 
+import dk.kea.dat19c.Library.java.repositories.BoegerRepositoryImpl;
+import dk.kea.dat19c.Library.java.repositories.IBoegerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
-@Controller
 
-public class boegerController {
+
+
+
+@Controller
+public class BoegerController {
+
+
+    private IBoegerRepository boegerRepository;
+
 
     @Autowired
+    public BoegerController(){
+        boegerRepository = new BoegerRepositoryImpl();
+    }
+
 
 
     @GetMapping("/findbog")
