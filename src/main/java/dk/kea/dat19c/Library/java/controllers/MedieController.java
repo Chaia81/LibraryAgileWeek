70ad6c632112d972140d06ae3dfea579e99cebe4
natalizacho <nata2716@stud.kea.dk>
2020-10-09 11:16:16 +0200

    .

---
+++ src/main/java/dk/kea/dat19c/Library/java/controllers/MedieController.java
@@ -0,0 +1,5 @@
+package dk.kea.dat19c.Library.java.controllers;
+
+public class MedieController {
+
+}
---
+++ src/main/java/dk/kea/dat19c/Library/java/repositories/IMedieRepository.java
@@ -0,0 +1,9 @@
+package dk.kea.dat19c.Library.java.repositories;
+
+import java.util.List;
+
+public interface IMedieRepository{
+
+
+
+}
